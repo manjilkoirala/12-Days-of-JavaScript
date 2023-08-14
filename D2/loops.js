cf1b7def591d0e26972b6e3d321f0561be906a6c
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-14 15:16:50 +0545

    for in and for of loop

--- D2/loops.js
+++ D2/loops.js
@@ -7,12 +7,19 @@ for(let i=0;i<groceries.length;i++){
 }
 
 
-//number
+//for of loop
 
 const numbers=[1,2,3,4,5,6];
 
-for(const number of numbers){
-    console.log(number)
+for(const keys of numbers){  //for of loop gives the element of arrays
+    console.log(keys)
+}
+
+//for in loop
+
+for(const keys in numbers){  
+    console.log(keys) //keys hold the index of element of array
+    console.log(numbers[keys]) //this will give the element of arrays
 }
 
 
@@ -25,4 +32,10 @@ while(j<groceries.length){
     console.log(groceries[j]);
     j++
 
+}
+
+let myString="This is me";
+
+for(const char of myString){  //for in loop gives the number of elements in arrays
+    console.log(char)
 }
