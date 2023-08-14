9cca026e2e31d36547c2ae0c5978db3305f3847b
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-14 14:31:29 +0545

    Separating Days

---
+++ D2/loops.js
@@ -0,0 +1,28 @@
+const groceries=['🍎','🍌','🥭','🍊'];
+
+//Using for loop
+
+for(let i=0;i<groceries.length;i++){
+    console.log(groceries[i]);
+}
+
+
+//number
+
+const numbers=[1,2,3,4,5,6];
+
+for(const number of numbers){
+    console.log(number)
+}
+
+
+
+//Using while loop
+
+let j=0;
+
+while(j<groceries.length){
+    console.log(groceries[j]);
+    j++
+
+}
