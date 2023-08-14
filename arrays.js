b6533f687a67bf7958c6253538972ab9aff3d42f
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-14 12:11:00 +0545

    Completed arrays before objects

--- arrays.js
+++ arrays.js
@@ -1,13 +1,31 @@
 //Defining array
 const groceries=['🍎','🍌','🥭','🍊'];
-console.log('Groceries:',groceries);
+
 console.log('Apple:',groceries[0]);
 console.log('Banana:',groceries[1]);
 console.log('Mango:',groceries[2]);
 console.log('Orange:',groceries[3]);
 
-//Using for loop in array
+//Using for loop in array(using length)
 
 for(let i=0;i<groceries.length;i++){
     console.log(groceries[i]);
 }
+
+//Using push
+
+groceries.push( '🍪'); //add cookies to the array at the end
+console.log('Groceries:',groceries);
+
+//Using pop
+
+groceries.pop(); //removes last added element from array(here cookies)
+console.log('Groceries:',groceries);
+
+//Slice in array
+
+console.log(groceries.slice(0,4)) //gives element from index 0 to 3
+
+//Using indexOf
+
+console.log(groceries.indexOf('🥭')); //gives index number of the element of array
