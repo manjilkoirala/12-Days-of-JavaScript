dcf5b1aec17e47e25aba282c8d90f49bacd9df61
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-14 11:45:48 +0545

    Array basic use

---
+++ arrays.js
@@ -0,0 +1,13 @@
+//Defining array
+const groceries=['🍎','🍌','🥭','🍊'];
+console.log('Groceries:',groceries);
+console.log('Apple:',groceries[0]);
+console.log('Banana:',groceries[1]);
+console.log('Mango:',groceries[2]);
+console.log('Orange:',groceries[3]);
+
+//Using for loop in array
+
+for(let i=0;i<groceries.length;i++){
+    console.log(groceries[i]);
+}
