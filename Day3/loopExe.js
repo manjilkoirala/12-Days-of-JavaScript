f038531c384f9ca359bb38bbda852e5b32af4b53
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-16 08:59:11 +0545

    Day3 Complete with loop exe

---
+++ Day3/loopExe.js
@@ -0,0 +1,70 @@
+
+
+//Finding Maximum number in an array
+const max=(numbers)=>{
+    let result=numbers[0];
+    for(const number of numbers){
+        if(number>result){
+            result=number;
+        }
+
+    }
+    return { result };
+}
+
+test=[1,2,3,6,5,1,5,6];
+console.log(max(test));
+
+//Making letterFrequency(haha=> {'h':2, 'a':2})
+
+const letterFrequency=(phrase)=>{
+    frequency={};
+    
+    for(const letter of phrase){
+       
+        if(letter in frequency){
+            
+        frequency[letter]++;
+        
+        }else{
+            frequency[letter]=1
+            
+    
+        }
+
+    }
+   return frequency
+
+}
+
+console.log(letterFrequency('hhaha'));
+
+
+///Making wordFrequency('hello Manjil hello')👉 {'hello':2, 'Manjil':1}
+
+const wordFrequency=(phrase)=>{
+    let frequencyObject={}
+    array= phrase.split(' ');
+    for(const words of array){
+        if(words in frequencyObject){
+            frequencyObject[words]++
+        }else{
+            frequencyObject[words]=1;
+        }
+    }
+    console.log(frequencyObject);
+
+}
+
+wordFrequency('hello Manjil hello')
+
+///Using previous function wordFrequency('hello Manjil hello')👉 {'hello':2, 'Manjil':1}
+
+const wordFrequencycheck=(phrase)=>{
+    
+    array= phrase.split(' ');
+    return letterFrequency(array);
+}
+
+console.log(wordFrequency('hello Manjil hello hello'))
+
