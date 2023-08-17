b0c72975a8b4a6cd2a80fb62b43f4ab6251e8d32
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-17 16:17:05 +0545

    Understand DOM and made Tip Calculator App

--- D5/Tip Calculator App/tipCalculator.js
+++ D5/Tip Calculator App/tipCalculator.js
@@ -5,19 +5,45 @@ const tipTotal=document.getElementById('tipInput')
 const numPeople=document.getElementById('numberOfPeople')
 const perPersonTotal=document.getElementById('perPersonTotal')
 
+//Number of People in number
+let numOfPeople= Number(numPeople.innerText)
 
 
 //calculateBill()
 
 function calculateBill(){
-    
+
+    //Tip Percentage
     let tipPercentage= (Number(tipTotal.value)/100)*(Number(billTotal.value))
-    actualTip=Number(billTotal.value) + tipPercentage;
-    afterPerson=actualTip/Number(numPeople.innerText)
-    perPersonTotal.innerText='$'+afterPerson;
-    if(billTotal.value==''&&tipTotal.value==''){
-        perPersonTotal.innerText='$0.00' 
+   
+    //total Amount
+    total=Number(billTotal.value) + tipPercentage;
+    afterPerson=total/numOfPeople
+
+
+    //Updating DOM
+    perPersonTotal.innerText=`$${afterPerson.toFixed(2)}`;
+   
+}
+
+//Increasing the number of increasePeople()
+
+function increasePeople(){
+    numOfPeople+=1
+    numPeople.innerHTML=numOfPeople
+    calculateBill()
+}
+
+//decreasePeople()
+
+function decreasePeople()
+{
+    if(numOfPeople==1){
+        return
     }
+    numOfPeople-=1
+    numPeople.innerHTML=numOfPeople
+    calculateBill()
 }
 
 
