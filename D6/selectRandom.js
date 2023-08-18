48b821b24d652ff1b23afee7dd2fdf860223d36d
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-18 18:40:03 +0545

    Random Fruit Generator

---
+++ D6/selectRandom.js
@@ -0,0 +1,13 @@
+
+
+const randomFruits=(fruits)=>{
+    
+    random=Math.floor(Math.random()*4)
+    return fruits[random]
+
+    
+}
+fruits=['🍎','🥭','🍊','🍌']
+
+
+fruit.innerText= randomFruits(fruits)
