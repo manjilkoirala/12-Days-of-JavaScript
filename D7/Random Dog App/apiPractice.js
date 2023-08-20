f0532848365e27ba09fba5d4a8ba18090fc3f783
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-20 21:24:21 +0545

    Random Dog App created

--- D7/Random Dog App/apiPractice.js
+++ D7/Random Dog App/apiPractice.js
@@ -1,8 +1,11 @@
+function fetchDog(){
 fetch('https://dog.ceo/api/breeds/image/random')
 .then(response=>response.json())
 .then(json=>randomDog(json.message))
-
+}
 
 randomDog=(dogs)=>{
-    document.getElementById("randomdog").src=dogs
+   document.getElementById("randomdog").src=dogs
+ 
+  
 }
