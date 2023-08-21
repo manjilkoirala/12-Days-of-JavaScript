629ee47c9eb6e3b77665aee92cc81cac2e57f8c7
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-21 21:19:57 +0545

    adding name and power of heros

--- D7/Superhero App/superHero.js
+++ D7/Superhero App/superHero.js
@@ -1,21 +1,38 @@
-function getSuperHero(name){
+function getSuperHerobyName(name){
     fetch(`https://superheroapi.com/api.php/2010389372655902/search/${name}`)
     .then(response=>response.json())
     .then(json=>{
-        let superhero=json.results[0].image.url
+        let superhero=json.results[0]
         console.log(superhero)
         hero=document.getElementById("image")
         hero.style.width="300px"
         hero.style.height="300px"
-        hero.src=superhero
+        hero.src=superhero.image.url
+        heroname.innerText=superhero.name
+        power.innerText="🧠Intelligence: "+ superhero.powerstats.intelligence
         
+    })}
+
+    function getRandomHero(){
+       number= Math.floor(Math.random()*732)
+       console.log(number)
+       fetch(`https://superheroapi.com/api.php/2010389372655902/${number}`)
+    .then(response=>response.json())
+    .then(json=>{
+        
+        hero=document.getElementById("image")
+        hero.style.width="300px"
+        hero.style.height="300px"
+        hero.src=json.image.url
+        heroname.innerText=  json.name
+        power.innerText="🧠Intelligence: "+json.powerstats.intelligence
     })
 
+    }
     
-}
 
 function getNewHero(){
 
     hero= document.getElementById('search').value
-getSuperHero(hero)
+getSuperHerobyName(hero)
 }
