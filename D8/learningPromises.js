17532af3721fae1c1031092f986aaecb55a7e0f9
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-22 23:11:18 +0545

    promise in js

--- D8/learningPromises.js
+++ D8/learningPromises.js
@@ -1,9 +1,17 @@
 //Async Code
 
-const waitingForSoup=()=>console.log('Hi Manjil after 2 sec')
+// const waitingForSoup=()=>console.log('Hi Manjil after 2 sec')
 
-setTimeout(waitingForSoup,10000)
+// setTimeout(waitingForSoup,10000)
 
 
 //Promise
-
+let myPromise = new Promise((myResolve, myReject)=> {
+    setTimeout(()=> { 
+        isReady=false
+        isReady?myResolve("I love You !!"):myReject("I dont love you") }, 3000)
+  });
+  
+  
+  
+console.log(myPromise.then(value=>console.log(value)).catch(err=>console.log(err)))
