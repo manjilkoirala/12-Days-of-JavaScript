1f89b8f095dcf8c59596ea71ef835522af5dbb35
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-08-23 21:59:35 +0545

    More example in promise, async,await

--- D9/asynAwait.js
+++ D9/asynAwait.js
@@ -14,16 +14,32 @@
 let myPromise = new Promise((myResolve, myReject)=> {
     setTimeout(()=> { 
         isReady=[true,false][Math.floor(Math.random()*2)]
-        isReady?myResolve("I love You !!"):myReject("I dont love you") }, 3000)
+        isReady?myResolve("Soup is Ready😋 !!"):myReject("No soup today😔") }, 3000)
   });
 
   async function exampleAsync(){
+    let summary=[]
     try{
  const ans=await myPromise
  console.log(ans)
+ summary.rating=5
+ summary.tip='Rs.100'
+ summary.pay='Rs 500'
+ console.log({summary})
     }catch(err){
         console.log(err)
+        summary.rating=1
+ summary.tip=0
+ summary.pay=0
+ console.log({summary})
     }
   }
 
   exampleAsync()
+
+
+  //Example
+
+  const sum= async(a,b)=>a+b
+
+  console.log( sum(1,2).then(value=>console.log(value)))
